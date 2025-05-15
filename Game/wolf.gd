fe7131e1ac7f693c71213b97627b15596661e467
Gayathri Poluri <gayathrisameeraa@gmail.com>
2025-05-15 12:40:57 -0700

    enemy scene added

---
+++ Game/wolf.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func _physics_process(delta):
+	velocity.y += gravity * delta
+
+	move_and_slide()
+func _on_player_detection_body_entered(body: Node2D) -> void:
+	if body.name=="player":
+		print("Player")
