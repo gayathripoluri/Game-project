7fd3a3aea23469090e59ffabd6a7aabfd8dac06a
Gayathri Poluri <gayathrisameeraa@gmail.com>
2025-05-22 18:28:40 -0700

    Gems added in assets folder

--- Game/Enemy/wolf.gd
+++ Game/Enemy/wolf.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var SPEED = 90
+var SPEED = 200
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var player
 var chase = false
--- Game/Player/player.gd
+++ Game/Player/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 400.0
 const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
